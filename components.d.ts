cec0d97df885bba6743442d2958bd74949bed457
jyc <80881099@qq.vom>
2022-10-04 00:40:53 +0800

    重构Qr显示

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    ElImage: typeof import('element-plus/es')['ElImage']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
     IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,22 +3,31 @@ import HelloWorld from "@/App.vue";
 import {nextTick, onMounted, reactive, ref} from "vue";
 import api from "@/api";
 import {ElMessage} from "element-plus";
-import QrcodeVue from 'qrcode.vue'
+import QRCode from "qrcode";
 
-let QrId = ref<string|null>()
-api.genQrCode().then(data=>{QrId.value=data.data.code})
-// console.log(QrId)
+let QrBase64 = ref<string | null>()
+api.genQrCode().then(data => {
+  QRCode.toDataURL("https://zuoye.lulufind.com/mp/qrcode/" + data.data.code, {
+    width: 260,
+    errorCorrectionLevel: "H"
+  }).then(url => {
+    QrBase64.value = url
+  }).catch(err => {
+    ElMessage.error("二维码生成错误请重试！")
+  })
+})
 
 </script>
 
 <template>
   <h1>Login in </h1>
   请使用手机微信或每日交作业小程序扫码
-<!--  <h1>{{QrId}}</h1>-->
-  <qrcode-vue :value='"https://zuoye.lulufind.com/mp/qrcode/"+QrId'/>
+  <el-image :src="QrBase64" fit="fill"/>
 </template>
 
-
 <style scoped>
-
+.el-image {
+  width: 260px;
+  height: 260px;
+}
 </style>
