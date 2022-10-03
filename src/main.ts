de7d2d848a059834ef371b3847b6464ea42a008b
jyc <80881099@qq.vom>
2022-10-03 23:53:01 +0800

    完成显示Qr

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
@@ -8,5 +7,4 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
