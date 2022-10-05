da95753d6bbe64844fca3c44cae3662825bf0dd6
jyc <80881099@qq.vom>
2022-10-05 09:33:00 +0800

    重构api

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,18 +3,14 @@ import {ElMessage} from "element-plus";
 
 const baseUrl = "https://api-prod.lulufind.com/api/v1"
 const api = {
-    async genQrCode(): Promise<any> {
-        let data = {};
-        await axios.post(`${baseUrl}/auth/genQrCode`, {}).then((rsp) => {
-            // console.log(rsp.data)
-            data = rsp.data
-            // console.log(data)
-        }).catch(() => {
-            ElMessage.error('二维码获取失败，请重新尝试')
+    genQrCode() {
+        return axios.post(`${baseUrl}/auth/genQrCode`, {})
+    },
+    checkQrCode(code:string){
+        // console.log({code})
+        return axios.post(`${baseUrl}/auth/checkQrCode`, {"code":code})
 
-        })
-        // console.log(data)
-        return data
+
     },
 }
 export default api;
