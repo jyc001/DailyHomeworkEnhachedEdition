9c695715b8ed2b945921d21ba210fb3b21627e20
jyc <80881099@qq.vom>
2022-10-05 09:33:17 +0800

    checkQr初步发送

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,18 +3,27 @@ import {ref} from "vue";
 import api from "@/api";
 import {ElMessage} from "element-plus";
 import QRCode from "qrcode";
-
 let QrBase64 = ref<string | null>()
-api.genQrCode().then(data => {
-  QRCode.toDataURL("https://zuoye.lulufind.com/mp/qrcode/" + data.data.code, {
+let tim1=0
+api.genQrCode().then(resp => {
+  // console.log(resp.data.data.code)
+  QRCode.toDataURL("https://zuoye.lulufind.com/mp/qrcode/" + resp.data.data.code, {
     width: 260,
     errorCorrectionLevel: "H"
   }).then(url => {
     QrBase64.value = url
+    tim1=setInterval(checkQrCode,2500,resp.data.data.code)
   }).catch(err => {
     ElMessage.error("二维码生成错误请重试！")
   })
 })
+
+const checkQrCode = function (code:string) {
+  // console.log(code)
+  api.checkQrCode(code)
+  console.log(tim1)
+  // clearTimeout(tim1)
+}
 </script>
 
 <template>
