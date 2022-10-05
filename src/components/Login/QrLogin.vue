d3562b38fa6ada24d0ff26dc881a17f50d6555ad
jyc <80881099@qq.vom>
2022-10-05 09:36:34 +0800

    拆组件 清理无用文件

---
+++ src/components/Login/QrLogin.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import {ref} from "vue";
+import api from "@/api";
+import QRCode from "qrcode";
+import {ElMessage} from "element-plus";
+
+let QrBase64 = ref<string | null>()
+let tim1=0
+api.genQrCode().then(resp => {
+  QRCode.toDataURL("https://zuoye.lulufind.com/mp/qrcode/" + resp.data.data.code, {
+    width: 260,
+    errorCorrectionLevel: "H"
+  }).then(url => {
+    QrBase64.value = url
+    tim1=setInterval(checkQrCode,2500,resp.data.data.code)
+  }).catch(err => {
+    ElMessage.error("二维码生成错误请重试！")
+  })
+})
+
+const checkQrCode = function (code:string) {
+  // console.log(code)
+  api.checkQrCode(code)
+  console.log(tim1)
+  // clearTimeout(tim1)
+}
+</script>
+
+<template>
+  请使用手机微信或每日交作业小程序扫码
+  <el-image :src="QrBase64" fit="fill"/>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,35 +1,11 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import api from "@/api";
-import {ElMessage} from "element-plus";
-import QRCode from "qrcode";
-let QrBase64 = ref<string | null>()
-let tim1=0
-api.genQrCode().then(resp => {
-  // console.log(resp.data.data.code)
-  QRCode.toDataURL("https://zuoye.lulufind.com/mp/qrcode/" + resp.data.data.code, {
-    width: 260,
-    errorCorrectionLevel: "H"
-  }).then(url => {
-    QrBase64.value = url
-    tim1=setInterval(checkQrCode,2500,resp.data.data.code)
-  }).catch(err => {
-    ElMessage.error("二维码生成错误请重试！")
-  })
-})
+import QrLogin from "@/components/Login/QrLogin.vue";
 
-const checkQrCode = function (code:string) {
-  // console.log(code)
-  api.checkQrCode(code)
-  console.log(tim1)
-  // clearTimeout(tim1)
-}
 </script>
 
 <template>
   <h1>Login in </h1>
-  请使用手机微信或每日交作业小程序扫码
-  <el-image :src="QrBase64" fit="fill"/>
+ <QrLogin/>
 </template>
 
 <style scoped>
