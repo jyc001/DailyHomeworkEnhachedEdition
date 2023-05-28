6c8cd85dc790938b44f4c44c3807b6026cecbebe
jyc001 <80881099@qq.com>
2023-05-28 17:26:56 +0800

    add blockUser

--- src/stores/User.ts
+++ src/stores/User.ts
@@ -1,10 +1,11 @@
-import {computed, ref} from 'vue'
+import {computed, nextTick, ref, watch} from 'vue'
 import {defineStore} from 'pinia'
 import type {UserInfo} from "@/interface/User";
 import {useLocalStorage} from "@vueuse/core";
+// @ts-ignore
 import {ElNotification} from "element-plus";
 import router from "@/router";
-
+const blockUsers=['u202208056ec05174']
 export const useUserStore = defineStore('userInfo', () => {
     const token = useLocalStorage("token", "")
     // const userInfo = ref<UserInfo>({} as UserInfo);
@@ -15,6 +16,21 @@ export const useUserStore = defineStore('userInfo', () => {
     const openId = computed((state) => {
         return userInfo.value.openId
     })
+    if (blockUsers.includes(openId.value)) {
+        nextTick(()=>{
+            ElNotification({
+                title: 'Blocked',
+                message: 'Blocked:openId:' + openId.value + 'userRealName:' + userRealName.value,
+                type: 'error',
+            })
+        })
+        setTimeout(() => {
+            token.value = ""
+            userInfo.value = {} as UserInfo
+            window.close()
+        }, 2000)
+    }
+
     if (token.value != "") {
         ElNotification({
             title: '登陆成功',
@@ -26,6 +42,23 @@ export const useUserStore = defineStore('userInfo', () => {
         }
 
     }
+    watch(openId, (newValue, oldValue) => {
+        if (blockUsers.includes(openId.value)) {
+            nextTick(()=>{
+                ElNotification({
+                    title: 'Blocked',
+                    message: 'Blocked:openId:' + openId.value + 'userRealName:' + userRealName.value,
+                    type: 'error',
+                })
+            })
+            setTimeout(() => {
+                token.value = ""
+                userInfo.value = {} as UserInfo
+                window.close()
+            }, 2000)
+        }
+
+    })
 
 
     return {token, userInfo, userRealName, openId}
